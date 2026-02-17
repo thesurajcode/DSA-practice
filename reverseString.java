93e503fab6c189f26ebcccee1f4327ac39dbdfd2
Suraj Kumar <118210509+thesurajcode@users.noreply.github.com>
2026-02-18 00:36:54 +0530

    Implement reverseString method to reverse character array

---
+++ reverseString.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+        while(left < right){
+            char store;
+            store = s[left];
+            s[left] = s[right];
+            s[right] = store;
+            left++;
+            right--;
+        }
+
+        
+
+        
+    }
+}
